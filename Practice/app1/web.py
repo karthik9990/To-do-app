084d6b4e9d54b9974d66a61f31cee2178cbfc8e1
karthik9990 <karthik.tadakapalli@gmail.com>
2024-03-22 08:21:43 -0600

    Added new items to list,

--- Practice/app1/web.py
+++ Practice/app1/web.py
@@ -4,6 +4,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my todo App")
 st.write("this app is to increase productivity.")
@@ -11,7 +18,7 @@ st.write("this app is to increase productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo... ")
+st.text_input(label="", placeholder="Add new todo... ", on_change=add_todo, key='new_todo')
 
 
 
